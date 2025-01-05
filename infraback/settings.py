af3a94746fdcfa2c111e5879dd787acd8f16a257
Mohamed-Ali-Bego <mohamedalibego11@gmail.com>
2025-01-05 23:00:20 +0200

    'f'

--- infraback/settings.py
+++ infraback/settings.py
@@ -128,26 +128,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-# STATIC_ROOT = BASE_DIR / 'static'
-# STATIC_URL = 'static/'
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/infratech/static/')
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = 'static/'
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, '/var/www/infratech/static/')
 STATICFILES_DIRS = [
     BASE_DIR / 'infraback/static'
 ]
 
 # Whitenoise static stuff
-STATICFILES_STORAG = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = BASE_DIR / '/var/www/infratech/static/'
+#STATICFILES_STORAG = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATIC_ROOT = BASE_DIR / '/var/www/infratech/static/'
 
 #Media Folder
-# MEDIA_ROOT = BASE_DIR / 'media'
-# MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
 
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/var/www/infratech/media/')
+#MEDIA_URL = '/media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR, '/var/www/infratech/media/')
+
+
 
 
 # Default primary key field type
